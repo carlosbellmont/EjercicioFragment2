f84754ad4e84f3f103ce5ffd19389a00edf8e84d
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-21 11:40:00 +0200

    Passing info

--- app/src/main/java/com/cbellmont/ejemploandroidfragment1/FragmentRojo.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidfragment1/FragmentRojo.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_rojo.*
 
 class FragmentRojo : Fragment() {
 
+    private var mainActivityFragmentInterface: MainActivityFragmentInterface? = null
+
+    fun setListener(mainActivityFragmentInterface: MainActivityFragmentInterface) {
+        this.mainActivityFragmentInterface = mainActivityFragmentInterface
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_rojo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        layoutRojo.setOnClickListener { mainActivityFragmentInterface?.onClick() }
     }
 
 }
--- app/src/main/java/com/cbellmont/ejemploandroidfragment1/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidfragment1/MainActivity.kt
@@ -2,20 +2,29 @@ package com.cbellmont.ejemploandroidfragment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    var nextFragment : Fragment = FragmentRojo()
+interface MainActivityFragmentInterface {
+    fun onClick()
+}
+
+class MainActivity : AppCompatActivity(), MainActivityFragmentInterface {
+    var nextFragment = FragmentRojo()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
+            nextFragment.setListener(this)
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.add(R.id.frameLayout, nextFragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
-            nextFragment = if (nextFragment is FragmentRojo) FragmentAmarillo() else FragmentRojo()
         }
+    }
+
+    override fun onClick() {
+        Toast.makeText(this, "Click recibido en MainActivity", Toast.LENGTH_LONG).show()
     }
 }

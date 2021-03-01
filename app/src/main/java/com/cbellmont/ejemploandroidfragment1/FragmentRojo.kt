31ec292a9f327e996cce7c78a708cca0e730cce9
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-03-01 16:12:13 +0100

    Updated to 1.4.31

--- app/src/main/java/com/cbellmont/ejemploandroidfragment1/FragmentRojo.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidfragment1/FragmentRojo.kt
@@ -5,23 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cbellmont.ejemploandroidfragment1.databinding.FragmentRojoBinding
 import kotlinx.android.synthetic.main.fragment_rojo.*
 
 class FragmentRojo : Fragment() {
 
     private var mainActivityFragmentInterface: MainActivityFragmentInterface? = null
 
+    private lateinit var binding : FragmentRojoBinding
+
     fun setListener(mainActivityFragmentInterface: MainActivityFragmentInterface) {
         this.mainActivityFragmentInterface = mainActivityFragmentInterface
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_rojo, container, false)
+        binding = FragmentRojoBinding.inflate(layoutInflater, container, false )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        layoutRojo.setOnClickListener { mainActivityFragmentInterface?.onClick() }
+        binding.layoutRojo.setOnClickListener { mainActivityFragmentInterface?.onClick() }
     }
 
 }
--- app/src/main/java/com/cbellmont/ejemploandroidfragment1/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidfragment1/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.cbellmont.ejemploandroidfragment1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.cbellmont.ejemploandroidfragment1.databinding.ActivityMainBinding
 
 interface MainActivityFragmentInterface {
     fun onClick()
 }
 
 class MainActivity : AppCompatActivity(), MainActivityFragmentInterface {
+
+    lateinit var binding: ActivityMainBinding
+
     var nextFragment = FragmentRojo()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
             nextFragment.setListener(this)
             val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.add(R.id.frameLayout, nextFragment)
+            fragmentTransaction.add(binding.frameLayout.id, nextFragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
